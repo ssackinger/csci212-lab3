5ffd418f9cbdc47f7592d92e795218fef49cc3d3
BuildTools <unconfigured@null.spigotmc.org>
2023-05-25 17:38:25 -0700

    wip

--- checkPrimeNumber.s
+++ checkPrimeNumber.s
@@ -19,18 +19,18 @@ unsigned_divide:
 */
 
 
-checkPrimeNumber                @r0 is incoming #, returns 1 or 0 in r0
+checkPrimeNumber:                @r0 is incoming #, returns 1 or 0 in r0
     push {r1, r2, r7, lr}
 
                                 @udiv r9, r0, #2
 
     ands r1, r0, #1
-    jeq checkPrimeNumber_notPrime
+    beq checkPrimeNumber_notPrime
 
     mov r7, #3                  @start testing at 3
 
 checkPrimeNumber_loop:
-    mult r1, r7, r7
+    mul r1, r7, r7
     cmp r1, r0
     ble checkPrimeNumber_testPrime
     mov r0, #1                  @set flag true
@@ -41,7 +41,7 @@ checkPrimeNumber_testPrime:     @calculate mod
     udiv r1, r0, r7
     mul r1, r1, r7
     subs r2, r0, r1
-    jeq checkPrimeNumber_notPrime
+    beq checkPrimeNumber_notPrime
 
     add r7, r7, #2
     b checkPrimeNumber_loop
