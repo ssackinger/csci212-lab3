cd6eb28f62bd7f466dc5a68229a2eca66f6535cb
BuildTools <unconfigured@null.spigotmc.org>
2023-05-25 17:48:31 -0700

    wip

--- checkPrimeNumber.s
+++ checkPrimeNumber.s
@@ -18,7 +18,8 @@ unsigned_divide:
     bx lr
 */
 
-EXPORT checkPrimeNumber
+global checkPrimeNumber
+
 checkPrimeNumber:                @r0 is incoming #, returns 1 or 0 in r0
     push {r1, r2, r7, lr}
 
